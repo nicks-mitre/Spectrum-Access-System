01d70ffedf43cc09b322563aa5c7d97af99c9ed0
Nick Stegman <nstegman@mitre.org>
2022-08-15 11:33:27 -0400

    Update certs/.gitignore; clean

--- src/harness/get_module_classes_methods.py
+++ src/harness/get_module_classes_methods.py
@@ -1,12 +1,11 @@
-from usefulPythonShellFunctions import *
-import typing, re
+# from usefulPythonShellFunctions import filterGlobalsListByType
+import typing, re, sys, copy, importlib
 from inspect import signature as get_signature
-from typing import get_type_hints, Optional, Union, Dict, List, Tuple, Set
+from typing import Optional, Union, Dict, List, Tuple, Any, Type
 from types import ModuleType, FunctionType
-import importlib
-import copy
 
-OptStr = Optional[str] # == Union[str, NoneType]
+OptStr = Optional[str]  # == Union[str, NoneType]
+
 
 def _accept_module_or_str(
 	module: Union[ModuleType, str],
@@ -29,8 +28,82 @@ def _accept_module_or_str(
 		mod = module
 	else:
 		raise TypeError(f"module must be of type ModuleType or str. Received type: {type(module)}")
-	
+
 	return mod
+
+
+def _getVarsInfoList(obj: Optional[Any] = None) -> List[Tuple[str, str, Union[str, int], int]]:
+	"""
+	Returns a list of tuples containing information about objects in one of the following:
+	global symbol table (if called without an argument - default)
+	object obj's __dict__ (if called with an argument)
+	"""
+	itemsList = []
+	infoList = []  # The list to be returned
+
+	if obj is None:
+		# If the function is called without args, the function uses the global symtable
+		itemsList = globals().items()
+	else:
+		# Otherwise, it uses the symtable of the obj
+		try:
+			itemsList = vars(obj).items()
+		except TypeError:
+			# TODO: Implement __slots__ check and return that instead.
+			# TypeError: vars() argument must have __dict__ attribute
+			return infoList
+
+	for k, v in itemsList:
+		vLen = '—'  # displayed length for values that aren't Sized
+
+		# typing.Sized is "an alias to collections.abc.Sized," which is an "[abstract base class] for classes
+		# that provide the __len__() method."
+		if isinstance(v, typing.Sized):
+			# vLen = str(len(v))
+			vLen = len(v)
+
+		sizeInBytes = sys.getsizeof(v)
+		tupToAppend = (k, type(v), vLen, sizeInBytes)
+		infoList.append(tupToAppend)
+
+	return infoList
+
+
+def _filterGlobalsListByType(
+	desiredType: Type,
+	obj: Optional[Any] = None,
+	include_subclasses: bool = True
+) -> List[Tuple[str, str, Union[str, int], int]]:
+	"""
+	Returns a list of tuples containing information about the (non-IPython-related) objects of type desiredType in the
+	globals dict. :param desiredType: A typing.Type object (e.g. 'desiredType=FunctionType' is equivalent to
+	'desiredType=type(getVarsInfoList)') that specifies which objects to include in the returned list. :param obj:
+	Optional parameter specifying that
+	"""
+	if not (isinstance(desiredType, Type) or isinstance(desiredType, typing._GenericAlias)):
+		raise TypeError("desiredType must be of type Type")
+
+	itemsList = []
+	if (obj is not None) and (len(_getVarsInfoList(obj)) > 0):
+		for tup in _getVarsInfoList(obj):
+			if not include_subclasses:
+				if isinstance(tup[1], desiredType):
+					itemsList.append(tup)
+			else:
+				# issubclass(TypeA, TypeA) is True
+				if issubclass(tup[1], desiredType):
+					itemsList.append(tup)
+
+	elif len(_getVarsInfoList()) > 0:
+		for tup in _getVarsInfoList():
+			if not include_subclasses:
+				if isinstance(tup[1], desiredType):
+					itemsList.append(tup)
+			else:
+				if issubclass(tup[1], desiredType):
+					itemsList.append(tup)
+
+	return itemsList
 
 
 def get_module_classes(
@@ -45,8 +118,8 @@ def get_module_classes(
 	:param sort: If True (default), returns the list of classes sorted in alphabetical order. Otherwise, the list is in the order that the classes were defined/imported.
 	"""
 	mod = _accept_module_or_str(module, reload_module)
-	
-	filt_classes = filterGlobalsListByType(obj=mod, desiredType=typing.Type)
+
+	filt_classes = _filterGlobalsListByType(obj=mod, desiredType=typing.Type)
 	classes = [t[0] for t in filt_classes]
 	if sort:
 		return sorted(classes)
@@ -64,20 +137,20 @@ def get_module_classes_methods(
 	:param reload_module: If True (False by default), then the function will reload the given module (which must have been previously imported) to use any changes to the module since it was imported.
 	"""
 	mod = _accept_module_or_str(module, reload_module)
-	
+
 	classes = get_module_classes(mod, False)
-	
-	cls_method_mapping = {}
-	
+
+	cls_method_mapping = { }
+
 	for i in classes:
-		filt_funcs = filterGlobalsListByType(
-			obj = getattr(mod, i),
-			desiredType = FunctionType
+		filt_funcs = _filterGlobalsListByType(
+			obj=getattr(mod, i),
+			desiredType=FunctionType
 		)
-		funcs = sorted( [t[0] for t in filt_funcs] )
-		
-		cls_method_mapping.update( {i: funcs} )
-	
+		funcs = sorted([t[0] for t in filt_funcs])
+
+		cls_method_mapping.update({ i: funcs })
+
 	return cls_method_mapping
 
 
@@ -91,21 +164,22 @@ def get_module_type_aliases(
 	:param reload_module: If True (False by default), then the function will reload the given module (which must have been previously imported) to use any changes to the module since it was imported.
 	"""
 	mod = _accept_module_or_str(module, reload_module)
-	
-	filt_aliases = filterGlobalsListByType(obj=mod, desiredType=typing._GenericAlias)
-	
-	# without sorting, the aliases are in the order they were defined, 
-	#   which allows us to substitute sub-components of more complex type hints 
+
+	filt_aliases = _filterGlobalsListByType(obj=mod, desiredType=typing._GenericAlias)
+
+	# without sorting, the aliases are in the order they were defined,
+	#   which allows us to substitute sub-components of more complex type hints
 	#   with the an alias matching that component.
 	aliases = [t[0] for t in filt_aliases]
-	
-	return {k:getattr(mod,k) for k in aliases}
 
-# for a string like 'typing.Dict[str, object.typing.List[typing.Dict]]', 
+	return { k: getattr(mod, k) for k in aliases }
+
+
+# for a string like 'typing.Dict[str, object.typing.List[typing.Dict]]',
 #   matches 'typing', 'object.typing', and 'typing'
 base_name_only_sub = (r'', re.compile(r'(?<!:[.:])[\w,.]+(?:\.(?!:\[))'))
 
-# matches the type hint equivalent to Optional[Type]: 
+# matches the type hint equivalent to Optional[Type]:
 #   Union[TYPE, NoneType], where TYPE is an arbitrary type
 optional_hint_sub = (r'Opt\1', re.compile(r'Union\[(?P<paramType>[\w\d,|\[\] ]+), NoneType\]'))
 
@@ -143,26 +217,26 @@ def mk_sig_sub_from_alias(
 	:returns: a tuple with two elements- a string and a compiled regex object.
 	"""
 	alias_sub = []
-	
+
 	# make <replace_with> the first element of the returned tuple
 	alias_sub.append(replace_with)
-	
+
 	alias_str = str(alias)
-	
-	# Use the substituion tuples in prelim_sub_map to replace type hints 
+
+	# Use the substituion tuples in prelim_sub_map to replace type hints
 	# 	appearing as a substring within alias_str with their equivalent type aliases
 	for t in list(prelim_sub_map):
 		alias_str = t[1].sub(t[0], alias_str)
-	
+
 	# Create a re.Pattern object from the type alias' definition
 	# 	escape all instances of square braces
 	re_pat_str = alias_str.replace('[', '\[').replace(']', '\]')
 	# 	compile resulting str into a regex Pattern
 	re_pat = eval(f"re.compile(r'{re_pat_str}')")
-	
+
 	# make the regex pattern the second element of the returned tuple
 	alias_sub.append(re_pat)
-	
+
 	return tuple(alias_sub)
 
 
@@ -179,23 +253,23 @@ def mk_module_alias_substitions(
 	"""
 	mod = _accept_module_or_str(module, reload_module)
 	aliases = get_module_type_aliases(mod, False)
-	
+
 	new_subs = list(copy.deepcopy(prelim_sub_map))
-	
-	for k,v in aliases.items():	
+
+	for k, v in aliases.items():
 		# skip the alias if it's not user-defined
 		if getattr(v, '_special', False):
 			continue
-		
+
 		new_subs.append(
 			mk_sig_sub_from_alias(v, k, prelim_sub_map=new_subs)
 		)
-	
+
 	return new_subs
-	
+
 
 def module_types(module):
-	return [t[0] for t in filterGlobalsListByType(obj=module, desiredType=typing.Type)]
+	return [t[0] for t in _filterGlobalsListByType(obj=module, desiredType=typing.Type)]
 
 
 # @typing.no_type_check
@@ -211,10 +285,10 @@ def format_signature(
 	:param include_visibility_indicator: If True (default), then prepend '+ ' or '- ' to the formatted signature to indicate that the function is "public" or "private" (in the case of a sunder or dunder function), respectively.
 	"""
 	sig_str = str(get_signature(func))
-	
+
 	for t in type_aliases:
 		sig_str = t[1].sub(t[0], sig_str)
-	
+
 	if include_visibility_indicator:
 		indicator = '-' if func.__name__.startswith('_') else '+'
 		return f"{indicator} {func.__name__}{sig_str}"
@@ -229,15 +303,15 @@ def _temporary_module_operation(
 ):
 	"""
 	Perform the 'func' operation on the given module without adding the module to the globals dict.
-	Note that this function does not reload modules or invalidate the module cache, so if you want 
-	to use new modifications to the given module after already having imported it, you need to first 
+	Note that this function does not reload modules or invalidate the module cache, so if you want
+	to use new modifications to the given module after already having imported it, you need to first
 	use importlib.reload() on the module.
 	:param func: A function with a single argument for the module on which to operate
 	:param module_name: The name of the module on which to operate
-	:param module_package: The 'package' field in a f"from {package} import {module}" relative import statement	
+	:param module_package: The 'package' field in a f"from {package} import {module}" relative import statement
 	"""
 	module = importlib.import_module(module_name, module_package)
-	
+
 	return func(module)
 
 
@@ -255,21 +329,21 @@ def get_module_method_sigs(
 	:param include_visibility_indicator: If True (default), then prepend '+ ' or '- ' to the formatted signature to indicate that the function is "public" or "private" (in the case of a sunder or dunder function), respectively.
 	"""
 	mod = _accept_module_or_str(module, reload_module)
-	
+
 	mod_methods = get_module_classes_methods(mod, reload_module=False)
-	
+
 	mod_sigs = dict.fromkeys(mod_methods.keys())
-	
+
 	subs = copy.deepcopy(type_aliases)
 	subs.extend(mk_module_alias_substitions(mod, reload_module=False))
-	
+
 	for k in mod_methods.keys():
-		mod_sigs[k] = dict.fromkeys( mod_methods[k] )
-	
-	for cls,mthds in mod_methods.items():
+		mod_sigs[k] = dict.fromkeys(mod_methods[k])
+
+	for cls, mthds in mod_methods.items():
 		for mthd in mthds:
 			fsig_arg = getattr(getattr(mod, f"{cls}"), f"{mthd}")
-			mod_sigs[cls].update( {f'{mthd}': format_signature(fsig_arg, subs)} )
-	
+			mod_sigs[cls].update({ f'{mthd}': format_signature(fsig_arg, subs) })
+
 	return mod_sigs
 
--- src/harness/test_runner.py
+++ src/harness/test_runner.py
@@ -1,9 +1,55 @@
 #!/bin/python3
-import unittest, sys, re, os, inspect, logging
+import unittest, sys, re, os, inspect, logging, importlib
 from functools import wraps
-from typing import List, Optional, Union, Tuple, Callable
+from typing import List, Optional, Union, Tuple, Callable, Dict, Type
 from types import ModuleType
-from get_module_classes_methods import _accept_module_or_str, get_module_classes
+from get_module_classes_methods import _accept_module_or_str, _filterGlobalsListByType, get_module_classes
+
+
+def _accept_module_or_str(
+	module: Union[ModuleType, str],
+	reload_module: bool = False
+) -> ModuleType:
+	"""
+	:param module: either a module object on which to operate, or the name of (path to) a module file on which to temporarily import
+	:param reload_module: If True (False by default), then the function will reload the given module (which must have been previously imported) to use any changes to the module since it was imported.
+	"""
+	if isinstance(module, str):
+		mod_name = module.replace('.py', '')
+		if mod_name in globals().keys():
+			if reload_module:
+				mod = importlib.reload(globals()[mod_name])
+			else:
+				mod = globals()[mod_name]
+		else:
+			mod = importlib.import_module(mod_name)
+	elif isinstance(module, ModuleType):
+		mod = module
+	else:
+		raise TypeError(f"module must be of type ModuleType or str. Received type: {type(module)}")
+
+	return mod
+
+
+def get_module_classes(
+	module: Union[ModuleType, str],
+	reload_module: bool = False,
+	sort: bool = True
+) -> List[str]:
+	"""
+	Returns a list of the classes defined in (or imported by) the given module.
+	:param module: either a module object on which to operate, or the name of (path to) a module file on which to temporarily import
+	:param reload_module: If True (False by default), then the function will reload the given module (which must have been previously imported) to use any changes to the module since it was imported.
+	:param sort: If True (default), returns the list of classes sorted in alphabetical order. Otherwise, the list is in the order that the classes were defined/imported.
+	"""
+	mod = _accept_module_or_str(module, reload_module)
+
+	filt_classes = _filterGlobalsListByType(obj=mod, desiredType=Type)
+	classes = [t[0] for t in filt_classes]
+	if sort:
+		return sorted(classes)
+	else:
+		return classes
 
 
 def temp_set_logger_level(
@@ -13,12 +59,13 @@ def temp_set_logger_level(
 	"""
 	A decorator to temporarily set the logging level of the given logger to 'level', call a function, and then reset the level back to its previous value.
 	"""
+
 	def deco_temp_disable_logging(func: Callable):
 		@wraps(func)
 		def wrapper_temp_disable_logging(*args, **kwargs):
 			# store the current level of logger
 			log_lvl = logger.level
-			
+
 			print(f"curr_level: {logger.level}")
 			# disable the logger for level 'level' and below
 			logger.setLevel(level)
@@ -29,24 +76,25 @@ def temp_set_logger_level(
 			logger.setLevel(log_lvl)
 			print(f"restored_lvl: {logger.level}")
 			return res
-		return wrapper_temp_disable_logging
-	return deco_temp_disable_logging
 
+		return wrapper_temp_disable_logging
+
+	return deco_temp_disable_logging
 
 
 def run_tests_by_name(test: str) -> unittest.TextTestRunner.resultclass:
 	"""
 	Run the given test(s) specified by the given name.
-	
+
 	"The name may resolve either to a module, a test case class, a test method within a test case class, or a callable object which returns a TestCase or TestSuite instance." (unittest.loadTestsFromName docstring)
 	"""
 	tloader = unittest.TestLoader()
-	
+
 	tsuite = tloader.loadTestsFromName(test)
-	print(tsuite)
-	
+	# print(tsuite)
+
 	ttr = unittest.TextTestRunner()
-	
+
 	return ttr.run(tsuite)
 
 
@@ -66,7 +114,7 @@ def get_testcase_modules(
 		files = [i for i in files if i.startswith(prefix)]
 	elif suffix:
 		files = [i for i in files if i.endswith(suffix)]
-	
+
 	# get the default path separator in the current environment
 	sep = os.path.sep
 	if prepend_path:
@@ -86,10 +134,10 @@ def modname_from_test_mthd(mthd_name: str) -> str:
 	"""
 	test_rgx = re.compile(r'^test_')
 	modname_rgx = re.compile(r'_\d.*$')
-	
+
 	base = test_rgx.sub('', modname_rgx.sub('', mthd_name))
 	if not base.endswith('_testcase'):
-	    base += '_testcase'
+		base += '_testcase'
 	return base
 
 
@@ -102,14 +150,14 @@ def get_testcase_classes_from_module(
 	Returns a list of testcase class names in the given module
 	:param mod: either a module object on which to operate, or the name of (path to) a module file on which to temporarily import
 	"""
-	
+
 	mod = _accept_module_or_str(module)
-	
+
 	all_classes = get_module_classes(mod, reload_module=False)
-	
+
 	# filter out imported classes
 	path = mod.__file__
-	
+
 	classes = []
 	for str_cls in all_classes:
 		# print(f"mname: {mod.__name__}; cls: {str_cls}")
@@ -117,17 +165,17 @@ def get_testcase_classes_from_module(
 		# inspect.getsourcefile(OBJ) errors if OBJ is a builtin
 		# so, we check if the class' name is in the __builtins__ module
 		# if (
-			# hasattr(__builtins__, str_cls) or
-			# str_cls in sys.builtin_module_names
+		# hasattr(__builtins__, str_cls) or
+		# str_cls in sys.builtin_module_names
 		# ):
-			# print(f"{str_cls = :}")
-			# continue
+		# print(f"{str_cls = :}")
+		# continue
 		try:
 			if inspect.getsourcefile(cls) == path:
 				classes.append(str_cls)
 		except TypeError:
 			continue
-		
+
 	return [i for i in classes if i.endswith("Testcase")]
 
 
@@ -141,18 +189,18 @@ def get_fully_qual_testnames_from_mod(
 	"""
 	mod = _accept_module_or_str(module)
 	test_classes = get_testcase_classes_from_module(mod)
-	
+
 	fq_testmethods = []
-	
+
 	tloader = unittest.TestLoader()
-	
+
 	for cls in test_classes:
 		for test_name in tloader.getTestCaseNames(getattr(mod, cls)):
 			if prefix:
 				fq_testmethods.append(f"{prefix}.{mod.__name__}.{cls}.{test_name}")
 			else:
 				fq_testmethods.append(f"{mod.__name__}.{cls}.{test_name}")
-	
+
 	return fq_testmethods
 
 
@@ -167,13 +215,13 @@ def get_all_testnames_from_testdir(
 	f"{prefix}.{module_name}.{testcase_class_name}.{test_method_name}"
 	"""
 	test_method_names = []
-	
-	t_modules = get_testcase_modules(path, prefix, suffix) 
+
+	t_modules = get_testcase_modules(path, prefix, suffix)
 	for mod in t_modules:
 		test_method_names.extend(
 			get_fully_qual_testnames_from_mod(mod, qualified_prefix)
 		)
-	
+
 	return test_method_names
 
 
@@ -182,21 +230,26 @@ def record_all_test_res_from_testdir(
 	prefix: str = "WINNF",
 	suffix: str = "_testcase.py",
 	qualified_prefix: Optional[str] = None
-) -> Tuple[List, ...]:
+) -> Tuple[Dict[str, str], ...]:
 	tests = get_all_testnames_from_testdir(path, prefix, suffix, qualified_prefix)
-	
-	passed = []
-	failed = []
-	
+
+	# results = {"passed": [], "failed": []}
+	results = { }
+	# names = {"passed": [], "failed": []}
+	text_results = { }
+
 	for test in tests:
-		try:
-			tr.run_tests_by_name(test)
-			passed.append(test)
-		except Exception as exc:
-			failed.append(test)
-			# errors.append((test, exc))
-	
-	return passed, failed	
+		res = run_tests_by_name(test)
+
+		if res.wasSuccessful():
+			results[test] = res
+			text_results[test] = "Passed"
+		else:
+			results[test] = res
+			text_results[test] = "Failed"
+
+	return results, text_results
+
 
 if __name__ == "__main__":
 	print(list(enumerate(sys.argv)))
